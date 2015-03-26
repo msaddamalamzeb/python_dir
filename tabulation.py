595bd32c8e3935345cdd8a87ef1db7a3363642b1
msaddamalamzeb <msaddamalamzeb@gmail.com>
2015-03-26 22:44:08 +0500

    reading CSV using OOP

--- tabulation.py
+++ tabulation.py
@@ -3,7 +3,7 @@ from tabulate import tabulate
 csv_file = open("van.csv")
 csv_file_read = csv.reader(csv_file)
 
-
+csv_file_read.close()
 #data = [["Name", "Age", "Weight"],["Salman","23","43"],["Ali","20","70"]]
 
 print tabulate(csv_file_read)
