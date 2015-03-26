85c40703913152f10fc6a05460492da6b1c52c38
msaddamalamzeb <msaddamalamzeb@gmail.com>
2015-03-26 19:09:34 +0500

    reading csv file using tabulation library

--- tabulation.py
+++ tabulation.py
@@ -1,5 +1,9 @@
+import csv
 from tabulate import tabulate
+csv_file = open("van.csv")
+csv_file_read = csv.reader(csv_file)
 
-data = [["Name", "Age", "Weight"],["Salman","23","43"],["Ali","20","70"]]
 
-print tabulate(data)
+#data = [["Name", "Age", "Weight"],["Salman","23","43"],["Ali","20","70"]]
+
+print tabulate(csv_file_read)
